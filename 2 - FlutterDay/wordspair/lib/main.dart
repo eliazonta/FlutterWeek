b76bd773546eb45b686adfd1dfd891757454f6cb
happyeliia <elia.zontaa@gmail.com>
2022-08-19 12:08:24 +0200

    small fixes

--- 2 - FlutterDay/wordspair/lib/main.dart
+++ 2 - FlutterDay/wordspair/lib/main.dart
@@ -4,6 +4,7 @@ import './random_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
